aee57091d46b25700c788b9dc207673042563704
Clemens Keusch <ckeusch@student.ethz.ch>
2025-04-03 11:55:00 +0200

    UCIHAR loading works

--- src/datasets/ucihar_dataset.py
+++ src/datasets/ucihar_dataset.py
@@ -2,20 +2,47 @@ import pandas as pd
 import numpy as np
 import torch
 import lightning as L
+from torch.utils.data import IterableDataset
 from torch.utils.data import DataLoader, Dataset
 from gluonts.dataset.field_names import FieldName
 from gluonts.dataset.common import ListDataset
 from gluonts.transform import (
-    Chain,
-    AddObservedValuesIndicator,
     InstanceSplitter,
     InstanceSampler,
     ExpectedNumInstanceSampler,
-    FieldName,
-    AddAgeFeature,  # Good feature to use when absolute time is unknown
-    # DO NOT USE AddTimeFeatures
+    TransformedDataset,
+    Chain,
+    Transformation,
 )
-from gluonts.torch.batchify import batchify
+
+
+class TransformedIterableDataset(IterableDataset):
+    """
+    A transformed iterable dataset that applies a transformation pipeline on-the-fly.
+
+    Parameters:
+    ----------
+    dataset : Dataset
+        The original dataset to transform.
+    transform : Transformation
+        The transformation pipeline to apply.
+    is_train : bool, optional, default=True
+        Whether the dataset is used for training.
+    """
+
+    def __init__(
+        self, dataset: Dataset, transform: Transformation, is_train: bool = True
+    ):
+        super().__init__()
+
+        self.transformed_dataset = TransformedDataset(
+            dataset,
+            transform,
+            is_train=is_train,
+        )
+
+    def __iter__(self):
+        return iter(self.transformed_dataset)
 
 
 def create_dataloader(
@@ -27,11 +54,11 @@ def create_dataloader(
     look_back_window: int,
     prediction_window: int,
 ) -> DataLoader:
-    start_time = pd.Timestamp(start_time, freq=freq)
-
-    subject_train = np.loadtxt(datadir + "subject_train.txt")
+    start_time = pd.Timestamp(start_time)
+    datadir += "train/"
+    subject_train = np.loadtxt(datadir + "subject_train.txt")[:, np.newaxis]
     x_train = np.loadtxt(datadir + "X_train.txt")
-    y_train = np.loadtxt(datadir + "y_train.txt")
+    y_train = np.loadtxt(datadir + "y_train.txt")[:, np.newaxis]
 
     combined = np.concatenate([subject_train, y_train, x_train], axis=1)
     df = pd.DataFrame(combined)
@@ -42,14 +69,15 @@ def create_dataloader(
     train_time_series = []
 
     for subject in df["subject"].unique():
-        target = df[df["subject"] == subject].iloc[2:]
-        action_feature = df[df["subject"] == subject]["action"]
+        target = df[df["subject"] == subject].iloc[:, 2].values.T
+        action_feature = df[df["subject"] == subject]["action"].values[:, np.newaxis].T
 
         data_entry = {
             FieldName.TARGET: target,
             FieldName.START: start_time,
             FieldName.ITEM_ID: subject,
             FieldName.FEAT_DYNAMIC_CAT: action_feature,
+            FieldName.OBSERVED_VALUES: target,
         }
         train_time_series.append(data_entry)
 
@@ -62,7 +90,10 @@ def create_dataloader(
         is_pad_field=FieldName.IS_PAD,
         start_field=FieldName.START,
         forecast_start_field=FieldName.FORECAST_START,
-        instance_sampler=sampler,
+        instance_sampler=ExpectedNumInstanceSampler(  # Sample strategy
+            num_instances=1.0,  # Try to sample 1 instance per time step on average
+            min_future=prediction_window,
+        ),
         past_length=look_back_window,
         future_length=prediction_window,
         time_series_fields=[
@@ -71,35 +102,64 @@ def create_dataloader(
         ],
         dummy_value=0.0,
     )
-    transformation = [instance_splitter]
+    transformation = Chain([instance_splitter])
 
-    transformed_train_ds = train_ds.transform(transformation, is_train=True)
-
-    train_dataloader = DataLoader(
-        transformed_train_ds,  # Use the eagerly transformed dataset/iterable
-        batch_size=32,  # Number of INSTANCES per batch
-        shuffle=True,  # Shuffle the generated instances for training
-        num_workers=0,  # Adjust as needed
-        collate_fn=batchify,  # Use GluonTS batchify or default PyTorch collate
+    transformed_dataset = TransformedIterableDataset(
+        train_ds, transformation, is_train=True
     )
-
-    return train_dataloader
+    """
+    train_dataloader = DataLoader(
+        transformed_dataset, batch_size=32, num_workers=0, collate_fn=batchify
+    )
+    """
+    return transformed_dataset
 
 
 class UCIHARDataset(Dataset):
     def __init__(
         self,
         datadir: str,
+        mode: str,
         look_back_window: int,
         prediction_window: int,
+        train_participants: list = [1, 3, 5, 6, 7, 8, 11, 14, 15, 16, 17, 19, 21, 22],
+        val_participants: list = [4, 5, 6],
         freq: str = "20L",
         start_time: str = "2000-01-01 00:00:00",
         sampler: InstanceSampler = None,
     ):
-        pass
+        self.look_back_window = look_back_window
+        self.prediction_window = prediction_window
+        self.window = look_back_window + prediction_window
+
+        datadir += mode + "/"
+
+        subject = np.loadtxt(datadir + f"subject_{mode}.txt")[:, np.newaxis]
+        x = np.loadtxt(datadir + f"X_{mode}.txt")
+        y = np.loadtxt(datadir + f"y_{mode}.txt")[:, np.newaxis]
+        combined = np.concatenate([subject, y, x], axis=1)
+
+        self.data = []
+        self.lengths = []
+        for train_participant in train_participants:
+            par_data = combined[np.where(combined[:, 0] == train_participant)]
+            length = len(par_data) - self.window + 1
+            self.lengths.append(length)
+            self.data.append(par_data)
+
+        self.cumulative_lengths = np.cumsum([0] + self.lengths)
+        self.total_length = self.cumulative_lengths[-1]
+
+    def __len__(self) -> int:
+        return self.total_length
 
     def __getitem__(self, idx: int) -> torch.Tensor:
-        pass
+        file_idx = np.searchsorted(self.cumulative_lengths, idx, side="right") - 1
+        index = idx - self.cumulative_lengths[file_idx]
+        window = self.data[file_idx][index : (index + self.window), 2:]
+        x = torch.from_numpy(window[: self.look_back_window])
+        y = torch.from_numpy(window[self.look_back_window :])
+        return x, y
 
 
 class UCIHARDataModule(L.LightningDataModule):
@@ -150,3 +210,14 @@ class UCIHARDataModule(L.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+
+if __name__ == "__main__":
+    datadir = (
+        "C:/Users/cleme/ETH/Master/Thesis/data/UCIHAR/UCI HAR Dataset/UCI HAR Dataset/"
+    )
+
+    dataset = UCIHARDataset(datadir, "train", 10, 5)
+
+    for i in range(len(dataset)):
+        x, y = dataset[i]
---
+++ src/datasets/wildppg_dataset.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+import torch
+import lightning as L
+
+from torch.utils.data import Dataset, DataLoader
+from sklearn.preprocessing import StandardScaler
+from pathlib import Path
+from typing import Tuple
+
+
+class WildPPGDataset(Dataset):
+    def __init__(
+        self,
+        path: Path,
+        look_back_window: int = 320,
+        prediction_window: int = 128,
+        cases: list = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    ):
+        self.look_back_window = look_back_window
+        self.prediction_window = prediction_window
+        self.window = look_back_window + prediction_window
+        self.arrays = []
+        self.lengths = [len(arr) - self.window + 1 for arr in self.arrays]
+        self.cumulative_lengths = np.cumsum([0] + self.lengths)
+        self.total_length = self.cumulative_lengths[-1]
+
+    def __len__(self) -> int:
+        return self.total_length
+
+    def __getitem__(self, idx: int) -> torch.Tensor:
+        file_idx = np.searchsorted(self.cumulative_lengths, idx, side="right") - 1
+        index = idx - self.cumulative_lengths[file_idx]
+        window = self.arrays[file_idx][index : (index + self.window)]
+        x = torch.Tensor(window.iloc[: self.look_back_window, 0].values)
+        y = torch.Tensor(window.iloc[self.look_back_window :, 0].values)
+
+        return x, y
+
+
+class WildPPGDataModule(L.LightningDataModule):
+    def __init__(
+        self,
+        data_dir: str,
+        batch_size: int = 32,
+        look_back_window: int = 128,
+        prediction_window: int = 64,
+    ):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.look_back_window = look_back_window
+        self.prediction_window = prediction_window
+
+        self.train_participants = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.val_participants = [10, 11, 12]
+        self.test_participants = [13, 14, 15]
+
+    def setup(self, stage: str):
+        if stage == "fit":
+            self.train_dataset = WildPPGDataset(
+                self.data_dir,
+                self.look_back_window,
+                self.prediction_window,
+                self.train_participants,
+            )
+            self.val_dataset = WildPPGDataset(
+                self.data_dir,
+                self.look_back_window,
+                self.prediction_window,
+                self.val_participants,
+            )
+        if stage == "test":
+            self.test_dataset = WildPPGDataset(
+                self.data_dir,
+                self.look_back_window,
+                self.prediction_window,
+                self.test_participants,
+            )
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+
+if __name__ == "__main__":
+    from tqdm import tqdm
+
+    path = Path("C:/Users/cleme/ETH/Master/Thesis/data/DaLiA/data/WildPPG/data/")
+    dataset = WildPPGDataset(path)
+    for i in tqdm(range(len(dataset))):
+        x, y = dataset[i]
+        break
