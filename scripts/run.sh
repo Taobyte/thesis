76760888a6b2bd16c48cebc501d5c3eef65a9854
Clemens Keusch <ckeusch@student.ethz.ch>
2025-04-10 13:45:42 +0200

    changed run.sh to sbatch format

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash 
+#SBATCH -n 1 
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=8:00:00
+#SBATCH --gpus=1
 
-
-# ressource specs
-NUM_WORKERS=8
-TIME=04:00:00
-MEM_PER_CPU=8G
-
-JOB="python main.py path=cluster"
-sbatch -n 1 --cpus-per-task "$NUM_WORKERS" --mem-per-cpu="$MEM_PER_CPU" --time="$TIME" -p gpu --gpus=1 --gres=gpumem:"$MEM_PER_GPU" --wrap="nvidia-smi;$JOB"
+nvidia-smi 
+python main.py path=cluster
