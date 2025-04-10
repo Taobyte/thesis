9a8494132d0e74c3b4cd56a8b51ac4b0d49a0a38
Clemens Keusch <ckeusch@student.ethz.ch>
2025-04-10 13:55:29 +0200

    changed num workers parameters

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash 
+
+
 #SBATCH -n 1 
-#SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8G
 #SBATCH --time=8:00:00
 #SBATCH --gpus=1
 
+NUM_WORKERS="$1"
+
 conda activate thesis
 nvidia-smi 
-python main.py path=cluster
+python main.py path=cluster num_workers=${NUM_WORKERS}
