e7c3bf94a79400b66284f09ea193262729ce6e2a
Clemens Keusch <ckeusch@student.ethz.ch>
2025-04-30 18:13:15 +0200

    loading conda env in bash script

--- scripts/tune.sh
+++ scripts/tune.sh
@@ -5,9 +5,12 @@
 #SBATCH --mem-per-cpu=24G
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=8
-#SBATCH --job-name=tune_$1
+#SBATCH --job-name=tune_job
 #SBATCH --output=/cluster/project/holz/ckeusch/tune_logs/%x_%j.out
 
+source ~/.bashrc
+conda activate thesissource ./bashrc
 
 module load eth_proxy
+
 python main.py --multirun hydra/sweeper=${1}_sweeper model=${1} tune=True overfit=False num_workers=8
