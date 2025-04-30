2fe66a1e4821e4be9bebad88e63229e25813fd15
Clemens Keusch <ckeusch@student.ethz.ch>
2025-04-30 18:02:47 +0200

    defined tuner space & added tune.sh script

---
+++ scripts/tune.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:24g
+#SBATCH --mem-per-cpu=24G
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --job-name=tune_$1
+#SBATCH --output=/cluster/project/holz/ckeusch/tune_logs/%x_%j.out
+
+
+module load eth_proxy
+python main.py --multirun hydra/sweeper=${1}_sweeper model=${1} tune=True overfit=False num_workers=8
