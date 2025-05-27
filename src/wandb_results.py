cbc36d6c4eefc3d96af093c62f83517b774af241
Clemens Keusch <ckeusch@student.ethz.ch>
2025-05-27 16:42:53 +0200

    adamshyper & gpt4ts ready to be trained

--- src/wandb_results.py
+++ src/wandb_results.py
@@ -14,6 +14,7 @@ metric_to_name = {
     "test_MSE": "Mean Squared Error",
     "test_MAE": "Mean Absolute Deviation",
     "test_cross_correlation": "Cross Correlation",
+    "test_dir_acc": "Directional Accuracy",
 }
 
 model_to_name = {
@@ -24,113 +25,13 @@ model_to_name = {
     "pattn": "PAttn",
     "simpletm": "SimpleTM",
     "elastst": "ElasTST",
+    "gp": "Gaussian Process",
+    "bnn": "Bayesian Neural Network",
+    "kalmanfilter": "Kalman Filter",
+    "linear": "Linear Regression",
+    "hmm": "Hidden Markov Model",
+    "xgboost": "XGBoost",
 }
-
-
-def create_loss_plots(dataframes, fig):
-    def create_mean_std(runs: list[pd.DataFrame]) -> pd.DataFrame:
-        values = {"train_loss": [], "val_loss": []}
-        for run in runs:
-            values["train_loss"].append(run["train_loss_epoch"].values)
-            values["val_loss"].append(run["val_loss_epoch"].values)
-            assert len(run["train_loss_epoch"].values) == len(
-                run["val_loss_epoch"].values
-            )
-
-        for k, v in values.items():
-            length = len(v[0])
-            for item in v:
-                assert length == len(item), (
-                    f"not the same length, {length} and {len(item)}"
-                )
-
-        values = {k: np.vstack(v) for k, v in values.items()}
-        df = pd.DataFrame()
-        df["train_loss_mean"] = np.mean(values["train_loss"], axis=0)
-        df["train_loss_std"] = np.std(values["train_loss"], axis=0)
-
-        df["val_loss_mean"] = np.mean(values["val_loss"], axis=0)
-        df["val_loss_std"] = np.std(values["val_loss"], axis=0)
-
-        return df
-
-    for col_idx, (model_name, runs) in enumerate(dataframes.items(), start=1):
-        df = create_mean_std(runs)
-        epochs = np.arange(1, len(df) + 1)
-        showlegend = col_idx == 1
-        model_name = model_to_name[model_name]
-        print(model_name)
-        fig.add_trace(
-            go.Scatter(
-                x=epochs,
-                y=df["train_loss_mean"],
-                mode="lines",
-                name=f"{model_name} (train)",
-                line=dict(color="blue"),
-                showlegend=showlegend,
-            ),
-            row=1,
-            col=col_idx,
-        )
-        # Plot train std band
-        fig.add_trace(
-            go.Scatter(
-                x=np.concatenate([epochs, epochs[::-1]]),
-                y=np.concatenate(
-                    [
-                        df["train_loss_mean"] + df["train_loss_std"],
-                        (df["train_loss_mean"] - df["train_loss_std"])[::-1],
-                    ]
-                ),
-                fill="toself",
-                fillcolor="rgba(0, 0, 255, 0.2)",
-                line=dict(color="rgba(255,255,255,0)"),
-                hoverinfo="skip",
-                showlegend=showlegend,
-            ),
-            row=1,
-            col=col_idx,
-        )
-
-        # Plot val mean
-        fig.add_trace(
-            go.Scatter(
-                x=epochs,
-                y=df["val_loss_mean"],
-                mode="lines",
-                name=f"{model_name} (val)",
-                line=dict(color="red"),
-                showlegend=showlegend,
-            ),
-            row=1,
-            col=col_idx,
-        )
-        # Plot val std band
-        fig.add_trace(
-            go.Scatter(
-                x=np.concatenate([epochs, epochs[::-1]]),
-                y=np.concatenate(
-                    [
-                        df["val_loss_mean"] + df["val_loss_std"],
-                        (df["val_loss_mean"] - df["val_loss_std"])[::-1],
-                    ]
-                ),
-                fill="toself",
-                fillcolor="rgba(255, 0, 0, 0.2)",
-                line=dict(color="rgba(255,255,255,0)"),
-                hoverinfo="skip",
-                showlegend=showlegend,
-            ),
-            row=1,
-            col=col_idx,
-        )
-
-    """
-    # Update y-axis titles
-    for i in range(1, 7):
-        fig.update_yaxes(title_text="Loss", row=1, col=i)
-    """
-    return fig
 
 
 def process_results(
@@ -138,20 +39,24 @@ def process_results(
     look_back_window: int,
     prediction_window: int,
     use_heart_rate: bool,
-    use_activity_info: bool,
-    start_time: str = "2025-4-25",
+    use_dynamic_features: bool,
+    use_static_features: bool,
+    start_time: str = "2025-5-25",
 ):
     print(
-        f"Filtering for \n dataset: {dataset} \n look_back_window: {look_back_window} \n prediction_window: {prediction_window} \n HR: {use_heart_rate} \n activity_info: {use_activity_info}"
+        f"Filtering for \n dataset: {dataset} \n look_back_window: {look_back_window} \n prediction_window: {prediction_window} \n HR: {use_heart_rate} \n Dynamic Features: {use_dynamic_features} \n Static Features: {use_static_features}"
     )
 
     group_name, run_name, tags = create_group_run_name(
         dataset,
         "",
         use_heart_rate,
-        use_activity_info,
         look_back_window,
         prediction_window,
+        use_dynamic_features,
+        use_static_features,
+        fold_nr=-1,  # does not matter, we only want group_name
+        fold_datasets=[],  # does not matter
     )
 
     filters = {
@@ -167,56 +72,21 @@ def process_results(
 
     print(f"Found {len(runs)} runs.")
 
+    assert len(runs) % 3 == 0, "Attention, length of runs is not divisible by 3!"
+
     metrics = defaultdict(list)
-    dataframes = defaultdict(list)
 
     for run in runs:
-        model_name = run.name.split("_")[1]
+        model_name = run.name.split("_")[3]
         summary = run.summary._json_dict
         filtered_summary = {
             k: summary[k]
             for k in summary
-            if k in ["test_MSE", "test_MAE", "test_cross_correlation"]
+            if k in ["test_MSE", "test_MAE", "test_cross_correlation", "test_dir_acc"]
         }
         metrics[model_name].append(filtered_summary)
-        history = run.scan_history()
-        train_loss = []
-        val_loss = []
-        for row in history:
-            if "train_loss_epoch" in row and row["train_loss_epoch"]:
-                train_loss.append(row["train_loss_epoch"])
-            if "val_loss_epoch" in row and row["val_loss_epoch"]:
-                val_loss.append(row["val_loss_epoch"])
-
-        loss_df = pd.DataFrame.from_dict(
-            {"train_loss_epoch": train_loss, "val_loss_epoch": val_loss}
-        )
-        dataframes[model_name].append(loss_df)
-
-    fig = make_subplots(
-        rows=2,
-        cols=6,
-        # First row: 6 individual plots
-        specs=[
-            [
-                {"type": "xy"},
-                {"type": "xy"},
-                {"type": "xy"},
-                {"type": "xy"},
-                {"type": "xy"},
-                {"type": "xy"},
-            ],
-            # Second row: single table spanning all columns
-            [{"type": "table", "colspan": 6}, None, None, None, None, None],
-        ],
-        subplot_titles=[model_to_name[model_name] for model_name in dataframes.keys()]
-        + ["Metrics Table"],
-        row_heights=[0.7, 0.3],  # 70% for plots, 30% for table
-        vertical_spacing=0.1,
-    )
 
     # fig = create_loss_plots(dataframes, fig)
-
     # plot metric table
     processed_metrics_mean = {}
     processed_metrics_std = {}
@@ -232,6 +102,9 @@ def process_results(
         processed_metrics_std[k] = std
     df = pd.DataFrame.from_dict(processed_metrics_mean)
     df_std = pd.DataFrame.from_dict(processed_metrics_std)
+    order = ["linear", "kalmanfilter", "gp", "xgboost", "simpletm", "pattn"]
+    df = df[order]
+    df_std = df_std[order]
 
     font_weights = []
     for i in range(len(df)):
@@ -245,11 +118,19 @@ def process_results(
         font_weights.append(weights)
 
     font_weights = [weight for row in font_weights for weight in row]
+    import pdb
+
+    pdb.set_trace()
 
     df = df.round(decimals=3)
     df.columns = [model_to_name[column] for column in df.columns]
     df_std.columns = [model_to_name[column] for column in df_std.columns]
 
+    fig = make_subplots(
+        rows=1,
+        cols=1,  # You can adjust this if you add loss curves later
+        specs=[[{"type": "table"}]],  # Specify the type of subplot for the table
+    )
     fig.add_trace(
         go.Table(
             header=dict(
@@ -275,17 +156,17 @@ def process_results(
                 ),
             ),
         ),
-        row=2,
+        row=1,
         col=1,
     )
     # Final layout
     fig.update_layout(
-        title="Loss Curves and Final Metrics",
-        # height=800,  # Adjust height as needed
-        # width=1200,
-        # xaxis_title="Step",
-        # yaxis_title="Loss",
-        # legend_title="Model",
+        title_text=f"Model Performance Metrics for {dataset.capitalize()} \n"
+        f"(Lookback: {look_back_window}, Prediction: {prediction_window}, \n"
+        f"HR: {use_heart_rate}, Dynamic Features: {use_dynamic_features}, Static Features: {use_static_features})",
+        title_x=0.5,  # Center the title
+        height=800,  # Adjust height dynamically based on number of rows
+        width=800 + len(df) * 100,  # Adjust width as needed
     )
 
     fig.show()
@@ -334,9 +215,16 @@ if __name__ == "__main__":
         help="get runs for heart rate only has an effect for datasets: dalia, ieee & ppg",
     )
     parser.add_argument(
-        "--use_activity_info",
+        "--use_dynamic_features",
         required=False,
         action="store_true",
+        help="get runs trained with activity information",
+    )
+    parser.add_argument(
+        "--use_static_features",
+        required=False,
+        action="store_true",
+        default=False,
         help="get runs trained with activity information",
     )
 
@@ -350,5 +238,6 @@ if __name__ == "__main__":
         args.look_back_window,
         args.prediction_window,
         args.use_heart_rate,
-        args.use_activity_info,
+        args.use_dynamic_features,
+        args.use_static_features,
     )
