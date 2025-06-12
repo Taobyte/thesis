6a3e50f0011a57ae5531c4057250934b9ef82bb5
Clemens Keusch <ckeusch@student.ethz.ch>
2025-06-12 11:50:12 +0200

    mend

--- scripts/sipml/tune.sh
+++ scripts/sipml/tune.sh
@@ -13,11 +13,11 @@ echo $PW
 
 torchrun --nproc_per_node=$GPUS \
     main.py \
-    --multirun hydra/sweeper="$MODEL_sweeper" \
-    model="$MODEL" \
-    dataset="$DATASET" \
-    look_back_window="$LBW" \
-    prediction_window="$PW" \
+    --multirun hydra/sweeper="$MODEL"_sweeper \
+    model=$MODEL \
+    dataset=$DATASET \
+    look_back_window=$LBW \
+    prediction_window=$PW \
     overfit=False \
     tune=True \ 
     use_wandb=False 
