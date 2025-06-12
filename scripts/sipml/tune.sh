5e383131d74c4e3ff363a13b17bfd8fb8518d651
Clemens Keusch <ckeusch@student.ethz.ch>
2025-06-12 11:43:43 +0200

    mend

--- scripts/sipml/tune.sh
+++ scripts/sipml/tune.sh
@@ -4,11 +4,11 @@ GPUS=1
 
 torchrun --nproc_per_node=$GPUS \
     main.py \
-    --multirun hydra/sweeper=${1}_sweeper \
-    model=${1} \
-    dataset=${2} \ 
-    look_back_window=${3} \ 
-    prediction_window=${4} \ 
+    --multirun hydra/sweeper="${1}_sweeper" \
+    model="${1}" \
+    dataset="${2}" \ 
+    look_back_window="${3}" \ 
+    prediction_window="${4}" \ 
     tune=True \ 
     overfit=False 
 
