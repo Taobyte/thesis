2c2f7e92748ee7b97e96d63479a75e7923cee2ac
Clemens Keusch <ckeusch@student.ethz.ch>
2025-06-24 10:42:28 +0200

    mend

--- scripts/euler/tune.sh
+++ scripts/euler/tune.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpumem:24g
 #SBATCH --mem-per-cpu=24G
 #SBATCH --time=04:00:00
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --job-name=tune_job
 #SBATCH --output=/cluster/project/holz/ckeusch/tune_logs/%x_%j.out
 
@@ -13,4 +13,4 @@ conda activate thesis
 
 module load eth_proxy
 
-nvidia-smi;python main.py --multirun hydra/sweeper=${1}_sweeper model=${1} normalization=${2} use_dynamic_features=${3} look_back_window=${4} prediction_window=${5} tune=True overfit=False num_workers=8
+nvidia-smi;python main.py --multirun hydra/sweeper=${1}_sweeper model=${1} normalization=${2} use_dynamic_features=${3} look_back_window=${4} prediction_window=${5} tune=True overfit=False
