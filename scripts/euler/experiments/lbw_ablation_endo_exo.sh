afa8084dd783c732ca4045e57c6029a4f535a6fd
Clemens Keusch <ckeusch@student.ethz.ch>
2025-08-11 15:14:12 +0200

    mend

--- scripts/euler/experiments/lbw_ablation_endo_exo.sh
+++ scripts/euler/experiments/lbw_ablation_endo_exo.sh
@@ -7,14 +7,14 @@ conda activate thesis
 # ressource specs
 TIME=24:00:00
 
-BLNNAME="endo_exo_bl_none"
+BLNNAME="ablation_endo_exo_bl_none"
 JOB="python main.py --multirun hydra/launcher=submitit_slurm dataset=dalia,wildppg,ieee lbw=a,b,c,d,e pw=a model=linear,hlinear,kalmanfilter,xgboost,hxgboost normalization=none use_wandb=True tune=False experiment=endo_exo folds=fold_0,fold_1,fold_2"
 sbatch --job-name="$BLNNAME" -o "$BLNNAME" --time="$TIME" --wrap="$JOB"
 
-BLGNAME="endo_exo_bl_global"
+BLGNAME="ablation_endo_exo_bl_global"
 JOB="python main.py --multirun hydra/launcher=submitit_slurm dataset=dalia,wildppg,ieee lbw=a,b,c,d,e pw=a model=gp,mlp normalization=global use_wandb=True tune=False experiment=endo_exo folds=fold_0,fold_1,fold_2"
 sbatch --job-name="$BLGNAME" -o "$BLGNAME" --time="$TIME" --wrap="$JOB"
 
-DLNAME="endo_exo_dl_global"
+DLNAME="ablation_endo_exo_dl_global"
 JOB="python main.py --multirun hydra/launcher=submitit_slurm dataset=dalia,wildppg,ieee lbw=a,b,c,d,e pw=a model=timesnet,simpletm,adamshyper,patchtst,timexer,gpt4ts normalization=global use_wandb=True tune=False experiment=endo_exo folds=fold_0,fold_1,fold_2"
 sbatch --job-name="$DLNAME" -o "$DLNAME" --time="$TIME" --wrap="$JOB"
