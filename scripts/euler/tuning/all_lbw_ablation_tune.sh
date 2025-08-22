c654f69bc7b0e927e018cae54b309a6e0c55ec0f
Clemens Keusch <ckeusch@student.ethz.ch>
2025-08-22 11:01:02 +0200

    mend

--- scripts/euler/tuning/all_lbw_ablation_tune.sh
+++ scripts/euler/tuning/all_lbw_ablation_tune.sh
@@ -1,8 +1,8 @@
 MODEL=${1}
 NORMALIZATION=${2}
 N_TRIALS=${3}
-LBWS=(5 10 20 30 60)
+LBWS=(a b c d e)
 for lbw in "${LBWS[@]}"
 do
-	sbatch scripts/euler/tuning/single_lbw_tune.sh "$MODEL" "$NORMALIZATION" "$N_TRIALS" "$lbw" 3
+	sbatch scripts/euler/tuning/single_lbw_tune.sh "$MODEL" "$NORMALIZATION" "$N_TRIALS" "$lbw" a
 done
