a9ec6832a1ad8e7303b19afdf08d02ac670a2b29
Clemens Keusch <ckeusch@student.ethz.ch>
2025-08-25 22:00:50 +0200

    fixed bug in bash local_all_lbw_ablation file, we need bash instead of sbatch

--- scripts/euler/tuning/local_all_lbw_ablation.sh
+++ scripts/euler/tuning/local_all_lbw_ablation.sh
@@ -1,3 +1,4 @@
+
 N_TRIALS=50
 BASELINES=(linear xgboost mole msar kalmanfilter gp mlp)
 DL=(timesnet simpletm adamshyper patchtst timexer gpt4ts nbeatsx)
@@ -6,11 +7,11 @@ for DATASET in "${DATASETS[@]}"
 do
     for MODEL in "${BASELINES[@]}"
     do
-        sbatch scripts/euler/tuning/local_lbw_ablation_tune.sh "$MODEL" difference "$N_TRIALS" "$DATASET"
+        bash scripts/euler/tuning/local_lbw_ablation_tune.sh "$MODEL" difference "$N_TRIALS" "$DATASET"
     done
 
     for MODEL in "${DL[@]}"
     do
-        sbatch scripts/euler/tuning/local_lbw_ablation_tune.sh "$MODEL" global "$N_TRIALS" "$DATASET"
+        bash scripts/euler/tuning/local_lbw_ablation_tune.sh "$MODEL" global "$N_TRIALS" "$DATASET"
     done
 done
