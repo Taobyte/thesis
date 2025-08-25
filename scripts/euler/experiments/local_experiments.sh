9ed8867693b0d5a4582bf3a0617079d6d0db486d
Clemens Keusch <ckeusch@student.ethz.ch>
2025-08-25 20:55:17 +0200

    mend

--- scripts/euler/experiments/local_experiments.sh
+++ scripts/euler/experiments/local_experiments.sh
@@ -9,5 +9,5 @@ TIME=24:00:00
 LBW=d # d = 30 
 
 NAME="local_runs"
-JOB="python main.py --multirun hydra/launcher=submitit_slurm dataset=ldalia,lwildppg,lieee lbw=lbw pw=a model=linear,mole,msar,kalmanfilter,gp,xgboost,mlp,timesnet,simpletm,adamshyper,patchtst,timexer,gpt4ts,nbeatsx normalization=global,difference use_wandb=True tune=False experiment=endo_exo folds=fold_0,fold_1,fold_2"
+JOB="python main.py --multirun hydra/launcher=submitit_slurm dataset=ldalia,lwildppg,lieee lbw=lbw pw=a model=linear,mole,msar,kalmanfilter,gp,xgboost,mlp,timesnet,simpletm,adamshyper,patchtst,timexer,gpt4ts,nbeatsx normalization=difference use_wandb=True tune=False experiment=endo_exo folds=fold_0,fold_1,fold_2"
 sbatch --job-name="$NAME" -o "$NAME_%j.out" --time="$TIME" --wrap="$JOB"
